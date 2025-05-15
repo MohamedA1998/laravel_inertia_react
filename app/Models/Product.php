11d4fd7fa4494016396ac180471b78526bacedf5
Mohamed Alsayed <malsayed493@gmail.com>
2025-05-15 17:14:09 +0300

    handel Product slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -13,4 +14,13 @@ class Product extends Model
 
     public static array $STATUS = ['active', 'inactive'];
     public static array $DISCOUNT_TYPE = ['percentage', 'fixed'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($product) {
+            $product->slug = Str::slug($product->name);
+        });
+    }
 }
