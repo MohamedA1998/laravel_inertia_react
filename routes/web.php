65a172418d9949cda2b4de90fb15a67aed7610cf
Mohamed Alsayed <malsayed493@gmail.com>
2025-05-15 15:26:48 +0300

    some update

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    sleep(3);
-    return inertia('Home', [
-        'name' => 'Mohamed atya',
-        'app' => env('APP_NAME')
-    ]);
-});
+Route::get('/', fn() => to_route('product.index'));
 
 Route::resource('product', ProductController::class);
