1656ef20eb6bf2f9a7858093b77d5d5599868cdf
Mohamed Alsayed <malsayed493@gmail.com>
2025-05-15 13:36:13 +0300

    handel react layout

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    sleep(3);
     return inertia('Home', [
         'name' => 'Mohamed atya',
         'app' => env('APP_NAME')
